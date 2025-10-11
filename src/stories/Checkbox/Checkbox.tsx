cb2fc3e2f041a2abf912a81bed092fd0e434bce1
JakeSaterlay <jakesaterlay@hotmail.co.uk>
2025-10-11 10:12:14 +0100

    label prop for checkbox

--- src/stories/Checkbox/Checkbox.tsx
+++ src/stories/Checkbox/Checkbox.tsx
@@ -1,8 +1,12 @@
-export const Checkbox = () => {
+interface CheckboxProps {
+  label: string;
+}
+
+export const Checkbox = ({ label }: CheckboxProps) => {
   return (
     <div className="rounded border bg-yellow-300 p-4 border-green-600 focus-within:bg-yellow-400">
       <input className="accent-green-600" type="checkbox" />
-      <label className="text-green-600 ml-2">Checkbox</label>
+      <label className="text-green-600 ml-2">{label}</label>
     </div>
   );
 };
